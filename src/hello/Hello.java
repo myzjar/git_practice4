6bd28dbb5e68a8ce6032dfdec29997a89e0b96b3
vasylmaziar <myzjar@gmail.com>
2022-04-21 14:32:11 +0300

    doing final work on hello feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("EU8 Learning Git and Github");
 
     }
 }
